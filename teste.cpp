5814bc4e6711fe87c9b42d4640717e94bb282c81
Eduardo Longhi <eduardo.longhi@laguz.com.br>
2024-07-29 15:09:03 -0300

    ex: cards pokemons

--- teste.cpp
+++ teste.cpp
@@ -1,28 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
 int main() {
-    int qtdPedacos, tamanhoDesejado;
-    cin >> qtdPedacos >> tamanhoDesejado;
-
-    int sanduiche[qtdPedacos];
-    for (int i=0; i<qtdPedacos; i++) cin >> sanduiche[i];
-
-    int qtdDeAlternativas = 0;
-    for (int i=0; i<qtdPedacos; i++) {
-        int tamanhoTotal = 0;
-        for (int j=0; j<qtdPedacos; j++) {
-            int pedaco = i+j;
-            if (pedaco >= qtdPedacos) pedaco -= qtdPedacos;
-            tamanhoTotal += sanduiche[pedaco];
-            if (tamanhoTotal == tamanhoDesejado) {
-                qtdDeAlternativas++;
-                break;
-            } else if (tamanhoTotal > tamanhoDesejado) break;
-        }
-    }
-
-    cout << qtdDeAlternativas << endl;
+    
 }
