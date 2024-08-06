410aacac0574a7615e1985a23c0399336c24cccb
Eduardo Longhi <eduardo.longhi@laguz.com.br>
2024-08-06 11:45:13 -0300

    ex: primos

---
+++ ex_feitos/otimizacoes/primos.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() { // O(log(n))
+    long long numero;
+    cin >> numero;
+
+    bool primo = numero != 1;
+    for (long long i=2; i<=numero/i; i+=2) {
+        if (numero%i == 0) {
+            primo = false;
+            break;
+        }
+        if (i == 2) i++;
+    }
+
+    if (primo) cout << "S" << endl;
+    else cout << "N" << endl;
+}
