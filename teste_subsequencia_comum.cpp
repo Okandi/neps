0050dbee0e47803ec79a705012a6fb40bdd8c6ef
Eduardo Longhi <eduardo.longhi@laguz.com.br>
2024-08-16 10:23:11 -0300

    atualizacao ex ortografia (parcial) +-

--- teste_subsequencia_comum.cpp
+++ teste_subsequencia_comum.cpp
@@ -3,49 +3,146 @@
 using namespace std;
 
 
-int main() {
+int main() { // FUNCIONA LOCAL, MAS NÃO NO NEPS >:( <sim, ta feio, eu sei>
     int qtdPalavrasDicionario, qtdPalavrasTestes;
     cin >> qtdPalavrasDicionario >> qtdPalavrasTestes;
 
-    string dicionario[qtdPalavrasDicionario];
-    for (int i=0; i<qtdPalavrasDicionario; i++) cin >> dicionario[i];
+    vector<string> palavrasDicionario;
+    for (int i=0; i<qtdPalavrasDicionario; i++) {
+        string palavraDicionario;
+        cin >> palavraDicionario;
+        palavrasDicionario.push_back(palavraDicionario);
+    }
 
-    vector<vector<string>> respostas;
+    vector<string> palavrasTeste;
     for (int i=0; i<qtdPalavrasTestes; i++) {
         string palavraTeste;
         cin >> palavraTeste;
+        palavrasTeste.push_back(palavraTeste);
+    }
 
-        vector<string> palavrasPossiveis;
-        for (int j = 0; j<qtdPalavrasDicionario; j++) {
-            string palavraDicionario = dicionario[j];
+    vector<vector<string>> respostas;
+    for (string palavraTeste : palavrasTeste) {
+        vector<string> possiveisCorrecoes;
 
-            int maiorSubsequencia[palavraDicionario.size()+1][palavraTeste.size()+1] = {};
+        for (string palavraDicionario : palavrasDicionario) {
+            int dp[palavraDicionario.size()+1][palavraTeste.size()+1] = {};
 
-            for (int oDicionario = 1; oDicionario <= palavraDicionario.size(); oDicionario++) {
-                for (int oTeste = 1; oTeste <= palavraTeste.size(); oTeste++) {
-                    if (palavraDicionario[oDicionario-1] == palavraTeste[oTeste-1]) {
-                        maiorSubsequencia[oDicionario][oTeste] = maiorSubsequencia[oDicionario-1][oTeste-1] + 1;
+            for (int i=1; i <= palavraDicionario.size(); i++) {
+                for (int j=1; j <= palavraTeste.size(); j++) {
+                    if (palavraDicionario[i-1] == palavraTeste[j-1]) {
+                        dp[i][j] = dp[i-1][j-1] + 1;
                     } else {
-                        maiorSubsequencia[oDicionario][oTeste] = max(maiorSubsequencia[oDicionario][oTeste-1], maiorSubsequencia[oDicionario-1][oTeste]);
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                     }
                 }
             }
 
-            int erros = (
-                abs((int)palavraDicionario.size() - maiorSubsequencia[palavraDicionario.size()][palavraTeste.size()])
-                + abs((int)palavraTeste.size() - maiorSubsequencia[palavraDicionario.size()][palavraTeste.size()])
-            );
+            string interseccao = "";
+            int i = palavraDicionario.size();
+            int j = palavraTeste.size();
+            while (i != 0 && j != 0) {
+                if (palavraDicionario[i-1] == palavraTeste[j-1]) {
+                    interseccao = palavraDicionario[i-1] + interseccao;
+                    i--; j--;
+                } else if (dp[i][j-1] >= dp[i-1][j]) {
+                    j--;
+                } else i--;
+            }
 
-            if (erros <= 2) palavrasPossiveis.push_back(palavraDicionario);
+            bool indiceDicionarioFazParteDaInterseccaoIndo[palavraDicionario.size()];
+            fill(indiceDicionarioFazParteDaInterseccaoIndo, indiceDicionarioFazParteDaInterseccaoIndo+palavraDicionario.size(), false);
+            i=0;
+            int k=0;
+            while (k<interseccao.size() && i<palavraDicionario.size()) {
+                if (palavraDicionario[i] == interseccao[k]) {
+                    indiceDicionarioFazParteDaInterseccaoIndo[i] = true;
+                    k++;
+                }
+                i++;
+            }
+
+            bool indiceTesteFazParteDaInterseccaoIndo[palavraTeste.size()];
+            fill(indiceTesteFazParteDaInterseccaoIndo, indiceTesteFazParteDaInterseccaoIndo+palavraDicionario.size(), false);
+            j=0;
+            k=0;
+            while (k<interseccao.size() && j<palavraTeste.size()) {
+                if (palavraTeste[j] == interseccao[k]) {
+                    indiceTesteFazParteDaInterseccaoIndo[j] = true;
+                    k++;
+                }
+                j++;
+            }
+
+            // for (bool val : indiceDicionarioFazParteDaInterseccaoIndo) cout << val << " ";
+            // cout << endl;
+
+            // for (bool val : indiceTesteFazParteDaInterseccaoIndo) cout << val << " ";
+            // cout << endl;
+
+            i=0; j=0;
+            int qtdErroIndo = 0;
+            while (i<palavraDicionario.size() && j<palavraTeste.size()) {
+                if (indiceDicionarioFazParteDaInterseccaoIndo[i] == indiceTesteFazParteDaInterseccaoIndo[j]) {
+                    if (!indiceDicionarioFazParteDaInterseccaoIndo[i]) qtdErroIndo++;
+                    i++; j++;
+                } else if (indiceDicionarioFazParteDaInterseccaoIndo[i] && !indiceTesteFazParteDaInterseccaoIndo[j]) {
+                    qtdErroIndo++;
+                    j++;
+                } else {
+                    qtdErroIndo++;
+                    i++;
+                }
+            }
+            qtdErroIndo += palavraDicionario.size()-i + palavraTeste.size()-j;
+
+
+            bool indiceDicionarioFazParteDaInterseccaoVoltando[palavraDicionario.size()];
+            fill(indiceDicionarioFazParteDaInterseccaoVoltando, indiceDicionarioFazParteDaInterseccaoVoltando+palavraDicionario.size(), false);
+            i=palavraDicionario.size()-1;
+            k=interseccao.size()-1;
+            while (0<=k && 0<=i) {
+                if (palavraDicionario[i] == interseccao[k]) {
+                    indiceDicionarioFazParteDaInterseccaoVoltando[i] = true;
+                    k--;
+                }
+                i--;
+            }
+
+            bool indiceTesteFazParteDaInterseccaoVoltando[palavraTeste.size()];
+            fill(indiceTesteFazParteDaInterseccaoVoltando, indiceTesteFazParteDaInterseccaoVoltando+palavraDicionario.size(), false);
+            j=palavraTeste.size()-1;
+            k=interseccao.size()-1;
+            while (0<=k && 0<=j) {
+                if (palavraTeste[j] == interseccao[k]) {
+                    indiceTesteFazParteDaInterseccaoVoltando[j] = true;
+                    k--;
+                }
+                j--;
+            }
+
+            i=0; j=0;
+            int qtdErroVoltando = 0;
+            while (i<palavraDicionario.size() && j<palavraTeste.size()) {
+                if (indiceDicionarioFazParteDaInterseccaoVoltando[i] == indiceTesteFazParteDaInterseccaoVoltando[j]) {
+                    if (!indiceDicionarioFazParteDaInterseccaoVoltando[i]) qtdErroVoltando++;
+                    i++; j++;
+                } else if (indiceDicionarioFazParteDaInterseccaoVoltando[i] && !indiceTesteFazParteDaInterseccaoVoltando[j]) {
+                    qtdErroVoltando++;
+                    j++;
+                } else {
+                    qtdErroVoltando++;
+                    i++;
+                }
+            }
+            qtdErroVoltando += palavraDicionario.size()-i + palavraTeste.size()-j;
+
+            if (qtdErroIndo <= 2 || qtdErroVoltando <= 2) possiveisCorrecoes.push_back(palavraDicionario);
         }
-
-        respostas.push_back(palavrasPossiveis);
+        respostas.push_back(possiveisCorrecoes);
     }
-
-    for (vector<string> palavrasPossiveis : respostas) {
-        for (string palavra : palavrasPossiveis) {
-            cout << palavra << " ";
-        }
+    for (vector<string> possiveisCorrecoes : respostas) {
+        for (string palavra : possiveisCorrecoes) cout << palavra << " ";
         cout << endl;
     }
 }
