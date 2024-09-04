5233a2b489cde3e8d32d51b8d9bdc4acca19f2bd
Eduardo Longhi <eduardo.longhi@laguz.com.br>
2024-09-04 11:07:09 -0300

    adicionou time complexity pro ex escalonamento otimo

--- ex_feitos/grafos/ordenacao_topologica/escalonamento_otimo.cpp
+++ ex_feitos/grafos/ordenacao_topologica/escalonamento_otimo.cpp
@@ -9,7 +9,7 @@ int main() {
 
 	vector<vector<int>> dependencias(qtdTarefas, vector<int>());
 
-	for (int i=0; i<qtdDependencias; i++) {
+	for (int i=0; i<qtdDependencias; i++) { // O(m)
 		int tarefaA, tarefaB;
 		cin >> tarefaA >> tarefaB;
 		dependencias[tarefaA].push_back(tarefaB);
@@ -19,24 +19,24 @@ int main() {
 
 	priority_queue<int, vector<int>, greater<int>> fila;
 
-	for (auto deps : dependencias)
+	for (auto deps : dependencias) // O(n+m)
 		for (auto tarefa : deps)
 			nDeps[tarefa]++;
 
-	for (int i=0; i<qtdTarefas; i++)
+	for (int i=0; i<qtdTarefas; i++) // O(n)
 		if (nDeps[i] == 0) fila.push(i);
 
 	vector<int> respostas;
 
-	while (!fila.empty()) {
+	while (!fila.empty()) { // O(n) --> O(m+n*log(n))
 		int tarefa = fila.top();
 		fila.pop();
 
 		respostas.push_back(tarefa);
 
-		for (auto tarefaDependente : dependencias[tarefa]) {
+		for (auto tarefaDependente : dependencias[tarefa]) { // O(m)
 			nDeps[tarefaDependente]--;
-			if (nDeps[tarefaDependente] == 0) fila.push(tarefaDependente);
+			if (nDeps[tarefaDependente] == 0) fila.push(tarefaDependente); // O(log n)
 		}
 	}
 
